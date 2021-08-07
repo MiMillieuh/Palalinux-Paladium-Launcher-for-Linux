5d36ad150ef4649fa5601c3fe34ef74d29852431
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2021-08-07 03:30:37 +0200

    Fix not starting in gnome

--- installer.sh
+++ installer.sh
@@ -9,7 +9,7 @@ WINEPREFIX=~/.WineApps/Paladium winecfg
 
 curl -L "https://drive.google.com/uc?export=download&id=1K7BxDSuCwyd-G-awdd5Rctc8fA4PDpjm" > PaladiumLauncher.tar.xz
 
-curl -L "https://drive.google.com/uc?export=download&id=1nZFN9a7qHI7vg3UiVE1wX_BJKGBpJX-3" > Assets.tar.xz
+curl -L "https://drive.google.com/uc?export=download&id=17QeWUUne6VijAu6V-m-4BRhmO3mkp08r" > Assets.tar.xz
 
 tar -xf PaladiumLauncher.tar.xz
 rm -rf PaladiumLauncher.tar.xz
@@ -17,9 +17,10 @@ mv Paladium\ Launcher ~/.WineApps/Paladium/drive_c
 rm -rf Paladium\ Launcher
 tar -xf Assets.tar.xz
 rm -rf Assets.tar.xz
+chmod +x launcher.sh
 wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 chmod +x winetricks
-WINEPREFIX=~/.WineApps/Paladium winetricks d3dcompiler_47 d3dx11_43
+WINEPREFIX=~/.WineApps/Paladium ./winetricks d3dcompiler_47 d3dx11_43
 
 mv launcher.sh ~/.WineApps/Paladium/drive_c
 mv Paladium.svg ~/.local/share/icons
