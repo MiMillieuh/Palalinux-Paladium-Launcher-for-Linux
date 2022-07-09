c221440d0662d4eae40b3e91a5687374f26ec7f2
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2022-07-10 00:40:52 +0200

    Preparation v8 Reborn

--- installer.sh
+++ installer.sh
@@ -3,9 +3,7 @@
 mkdir ~/.WineApps
 mkdir ~/.WineApps/Paladium
 
-zenity --info --text="Please set the windows version to Windows 7 then Apply and OK"
-
-WINEPREFIX=~/.WineApps/Paladium winecfg
+WINEPREFIX=~/.WineApps/Paladium wineboot
 
 curl -L "https://drive.google.com/uc?export=download&id=1K7BxDSuCwyd-G-awdd5Rctc8fA4PDpjm" > PaladiumLauncher.tar.xz
 
@@ -20,7 +18,7 @@ rm -rf Assets.tar.xz
 chmod +x launcher.sh
 wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 chmod +x winetricks
-WINEPREFIX=~/.WineApps/Paladium ./winetricks d3dcompiler_47 d3dx11_43
+WINEPREFIX=~/.WineApps/Paladium ./winetricks d3dcompiler_47 d3dx11_43 win7
 
 mv launcher.sh ~/.WineApps/Paladium/drive_c
 mv Paladium.svg ~/.local/share/icons
@@ -28,4 +26,3 @@ mv Paladium.desktop ~/.local/share/applications
 
 rm -rf winetricks
 rm -rf winetricks.1
-zenity --info --text="Installation finished, Have fun with Paladium ! (You might have some low framrate when starting the game let it for like a minute and you'll be fine. Take a little long to load too)"
